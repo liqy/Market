9367265e55fbe4c8819f76cd319d1afbb69f17d7
liqy <liqingyi@outlook.com>
2016-06-23 14:34:04 +0800

    Init

--- app/src/main/java/com/insthub/ecmobile/EcmobileApp.java
+++ app/src/main/java/com/insthub/ecmobile/EcmobileApp.java
@@ -14,35 +14,75 @@ package com.insthub.ecmobile;
 //
 
 import android.graphics.Bitmap;
+import android.util.Log;
+
 import com.insthub.BeeFramework.BeeFrameworkApp;
+import com.liulishuo.filedownloader.FileDownloader;
+import com.liulishuo.filedownloader.util.FileDownloadHelper;
+import com.liulishuo.filedownloader.util.FileDownloadLog;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 
+import java.net.Proxy;
+import java.util.concurrent.TimeUnit;
 
-public class EcmobileApp extends BeeFrameworkApp
-{
-    public static DisplayImageOptions options;		// DisplayImageOptions是用于设置图片显示的类
-    public static DisplayImageOptions options_head;		// DisplayImageOptions是用于设置图片显示的类
+import okhttp3.OkHttpClient;
+
+
+public class EcmobileApp extends BeeFrameworkApp {
+    public static DisplayImageOptions options;        // DisplayImageOptions是用于设置图片显示的类
+    public static DisplayImageOptions options_head;        // DisplayImageOptions是用于设置图片显示的类
+
     @Override
     public void onCreate() {
         super.onCreate();
 
+        initDownLoad();
+
         options = new DisplayImageOptions.Builder()
-                .showStubImage(R.drawable.default_image)			// 设置图片下载期间显示的图片
-                .showImageForEmptyUri(R.drawable.default_image)	// 设置图片Uri为空或是错误的时候显示的图片
-                .showImageOnFail(R.drawable.default_image)		// 设置图片加载或解码过程中发生错误显示的图片
-                .cacheInMemory(true)						// 设置下载的图片是否缓存在内存中
-                .cacheOnDisc(true)							// 设置下载的图片是否缓存在SD卡中
-                        //.displayer(new RoundedBitmapDisplayer(20))	// 设置成圆角图片
+                .showStubImage(R.drawable.default_image)            // 设置图片下载期间显示的图片
+                .showImageForEmptyUri(R.drawable.default_image)    // 设置图片Uri为空或是错误的时候显示的图片
+                .showImageOnFail(R.drawable.default_image)        // 设置图片加载或解码过程中发生错误显示的图片
+                .cacheInMemory(true)                        // 设置下载的图片是否缓存在内存中
+                .cacheOnDisc(true)                            // 设置下载的图片是否缓存在SD卡中
+                //.displayer(new RoundedBitmapDisplayer(20))	// 设置成圆角图片
                 .bitmapConfig(Bitmap.Config.RGB_565)
                 .build();
 
         options_head = new DisplayImageOptions.Builder()
-                .showStubImage(R.drawable.profile_no_avarta_icon)			// 设置图片下载期间显示的图片
-                .showImageForEmptyUri(R.drawable.profile_no_avarta_icon)	// 设置图片Uri为空或是错误的时候显示的图片
-                .showImageOnFail(R.drawable.profile_no_avarta_icon)		// 设置图片加载或解码过程中发生错误显示的图片
-                .cacheInMemory(true)						// 设置下载的图片是否缓存在内存中
-                .cacheOnDisc(true)							// 设置下载的图片是否缓存在SD卡中
-                        //.displayer(new RoundedBitmapDisplayer(30))	// 设置成圆角图片
+                .showStubImage(R.drawable.profile_no_avarta_icon)            // 设置图片下载期间显示的图片
+                .showImageForEmptyUri(R.drawable.profile_no_avarta_icon)    // 设置图片Uri为空或是错误的时候显示的图片
+                .showImageOnFail(R.drawable.profile_no_avarta_icon)        // 设置图片加载或解码过程中发生错误显示的图片
+                .cacheInMemory(true)                        // 设置下载的图片是否缓存在内存中
+                .cacheOnDisc(true)                            // 设置下载的图片是否缓存在SD卡中
+                //.displayer(new RoundedBitmapDisplayer(30))	// 设置成圆角图片
                 .build();
+    }
+
+    private void initDownLoad() {
+
+        Log.d(getPackageName(), "FileDownloader");
+
+        // just for open the log in this demo project.
+        FileDownloadLog.NEED_LOG = true;
+
+        /**
+         * just for cache Application's Context, and ':filedownloader' progress will NOT be launched
+         * by below code, so please do not worry about performance.
+         * @see FileDownloader#init(Context)
+         */
+        FileDownloader.init(getApplicationContext(),
+                new FileDownloadHelper.OkHttpClientCustomMaker() { // is not has to provide.
+                    @Override
+                    public OkHttpClient customMake() {
+                        // just for OkHttpClient customize.
+                        final OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                        // you can set the connection timeout.
+                        builder.connectTimeout(15_000, TimeUnit.MILLISECONDS);
+                        // you can set the HTTP proxy.
+                        builder.proxy(Proxy.NO_PROXY);
+                        // etc.
+                        return builder.build();
+                    }
+                });
     }
 }
--- app/src/main/java/tv/tipsee/vr/VRVideoInfoActivity.java
+++ app/src/main/java/tv/tipsee/vr/VRVideoInfoActivity.java
@@ -4,12 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.insthub.ecmobile.R;
+import com.liulishuo.filedownloader.BaseDownloadTask;
+import com.liulishuo.filedownloader.FileDownloadSampleListener;
+import com.liulishuo.filedownloader.FileDownloader;
+import com.liulishuo.filedownloader.util.FileDownloadUtils;
 import com.squareup.picasso.Picasso;
 import tv.tipsee.vr.models.VRVideo;
 import tv.tipsee.vr.player.MD360PlayerActivity;
@@ -69,7 +74,6 @@ public class VRVideoInfoActivity extends BaseActivity implements View.OnClickLis
                 .placeholder(R.drawable.default_image)
                 .error(R.drawable.default_image)
                 .into(vr_image);
-
     }
 
     @Override
@@ -79,6 +83,7 @@ public class VRVideoInfoActivity extends BaseActivity implements View.OnClickLis
                 finish();
                 break;
             case R.id.vr_video_download:
+                createDownloadTask(vrVideo.file).start();
                 break;
             case R.id.vr_video_play:
                 if (vrVideo != null) {
@@ -88,6 +93,50 @@ public class VRVideoInfoActivity extends BaseActivity implements View.OnClickLis
             default:
                 break;
         }
+    }
+
+    private Uri getVrUri(String url) {
+        return Uri.parse("file://" + getVideoFilePath(url));
+    }
+
+    private String getVideoFilePath(String url) {
+        return FileDownloadUtils.getDefaultSaveRootPath() + url.substring(url.lastIndexOf("/"));
+    }
+
+    private BaseDownloadTask createDownloadTask(String url) {
+        return FileDownloader.getImpl().create(url)
+                .setPath(getVideoFilePath(url))
+                .setCallbackProgressTimes(300)
+                .setMinIntervalUpdateSpeed(400)
+                .setListener(new FileDownloadSampleListener() {
+                    @Override
+                    protected void progress(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+                        super.progress(task, soFarBytes, totalBytes);
+
+                    }
+
+                    @Override
+                    protected void error(BaseDownloadTask task, Throwable e) {
+                        super.error(task, e);
+                    }
+
+                    @Override
+                    protected void paused(BaseDownloadTask task, int soFarBytes, int totalBytes) {
+                        super.paused(task, soFarBytes, totalBytes);
+                    }
+
+                    @Override
+                    protected void completed(BaseDownloadTask task) {
+                        super.completed(task);
+                        Log.i(getLocalClassName(), "OK");
+                        MD360PlayerActivity.startVideo(VRVideoInfoActivity.this, getVrUri(vrVideo.file));
+                    }
+
+                    @Override
+                    protected void warn(BaseDownloadTask task) {
+                        super.warn(task);
+                    }
+                });
     }
 
     @Override
